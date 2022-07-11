adb7ec8efded27531ca14ff89e841229a1fdd3a5
chinaza <hi@chinaza.dev>
2022-07-12 00:54:56 +0100

    feat: expose functions

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import { credentials } from '@grpc/grpc-js';
+import protoLoader from '@grpc/proto-loader';
+import GrpcClient from './client';
+import GrpcServer from './server';
+
+interface ProtoParams {
+  protoDir?: string;
+  protoOptions?: protoLoader.Options;
+}
+
+interface ServerParams extends ProtoParams {}
+
+interface ClientParams extends ProtoParams {
+  uri?: string;
+}
+
+export type {
+  ServerUnaryCall,
+  ServerReadableStream,
+  sendUnaryData,
+  ServerWritableStream
+} from '@grpc/grpc-js';
+
+export function Server(params: ServerParams) {
+  const server = GrpcServer.getInstance(params);
+
+  return {
+    addService: server.addService.bind(server),
+    listen: server.listen.bind(server)
+  };
+}
+
+export function Client(params: ClientParams) {
+  const { protoDir, protoOptions } = params;
+  const client = new GrpcClient({
+    protoDir,
+    protoOptions
+  });
+  return client;
+}
+
+export function getService(
+  client: GrpcClient,
+  serviceName: string,
+  uri?: string
+) {
+  const Service = client.getService(serviceName);
+
+  uri = uri || client.uri;
+  if (!uri) throw new Error('Invalid uri passed');
+
+  const service = new Service(uri, credentials.createInsecure());
+  return service;
+}
